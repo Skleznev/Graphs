6e733be871ddca2f2b19280f9259c5cdd4e4c6b2
Skleznev <job.skleznev@gmail.com>
2020-03-06 06:43:53 +0500

    Чтение матрицы

--- Graphs 1/Graphs 1.cpp
+++ Graphs 1/Graphs 1.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include<vector>
 
 using namespace std;
 
 class Graph
 {
+private:
+	vector <vector <int> > graph;
 public:
 	void readGraph(string fileName) {
-		char type[50];
+		char type[4];
 		int N, D, W;
 
 		ifstream in(fileName);
@@ -17,14 +20,21 @@ public:
 
 
 		if ('C') {
+			int weight;
 			cout << type;
-
+			for (int i = 0; i < N; i++) {
+				graph.push_back(vector<int>());
+				for (int j= 0; j < N; j++) {
+					graph[graph.size() - 1].push_back(0);
+					in >> weight;
+					addEdge(i, j, weight);
+				}
+			}
 		}
-
 	}
 
 	void addEdge(int from, int to, int weight) {
-
+		graph[from][to] = weight;
 	}
 
 	void removeEdge(int from, int to) {
